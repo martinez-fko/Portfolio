41a6bbb3f75cf6070d6b75b6dcf02ad447c21fc4
FranciscoWork <jose_fko_92@hotmail.com>
2022-08-26 15:23:54 -0500

    section portfolio 3D

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,7 +4,51 @@ const Portfolio = () => {
   return (
     <section className="portfolio" id="portfolio">
       <h2>Portfolio</h2>
-      <div className="portfolio-item">
+      <div className="portfolio__container">
+        <div className="portfolio__box">
+          <div className="portfolio__imgBx">
+            <img src="/Project_pokedex.png" alt="Proyecto Pokedex" />
+          </div>
+          <div className="portfolio__content">
+            <div>
+              <h2>Pokedex</h2>
+              <p>
+                Tecnologías usadas <br /> React | Redux | JavaScript | HTML5 |
+                CSS3
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio__box">
+          <div className="portfolio__imgBx">
+            <img src="/Project_pokedex.png" alt="Proyecto Pokedex" />
+          </div>
+          <div className="portfolio__content">
+            <div>
+              <h2>Pokedex</h2>
+              <p>
+                Tecnologías usadas <br /> React | Redux | JavaScript | HTML5 |
+                CSS3
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio__box">
+          <div className="portfolio__imgBx">
+            <img src="/Project_pokedex.png" alt="Proyecto Pokedex" />
+          </div>
+          <div className="portfolio__content">
+            <div>
+              <h2>Pokedex</h2>
+              <p>
+                Tecnologías usadas <br /> React | Redux | JavaScript | HTML5 |
+                CSS3
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="portfolio-item">
         <h3>Pokedex</h3>
         <a target={"_blank"}  href="https://pokedex-fcomtz.netlify.app/#/pokedex">
           <img src="/Project_pokedex.png" alt="" />
@@ -21,7 +65,7 @@ const Portfolio = () => {
         <a target={"_blank"} href="https://crudcontacts-fcomtz.netlify.app/">
           <img src="/Project_crud-user.png" alt="" />
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
